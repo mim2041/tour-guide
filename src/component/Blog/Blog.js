b70d5738ab5fed44f8e6d3b2abd8b524dbb7f428
mim2041 <suraiyamim338@gmail.com>
2023-04-01 02:26:16 +0600

    website completed

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -1,40 +1,60 @@
-import React from 'react';
-import './Blog.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faBookBookmark } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import "./Blog.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = (props) => {
-    const {id, title, author_name, author_img, publish_date, img_url, time_to_read} = props.blog;
-    const timeCalculation = props.timeCalculation;
-    const bookmarked = props.bookmarked;
-    
-    return (
-        <div className='blog'>
-            <div>
-                <img src={img_url} alt="" />
+  const { handleBookmark,countTime } = props;
+  const {
+    id,
+    title,
+    author_name,
+    author_img,
+    publish_date,
+    img_url,
+    time_to_read,
+  } = props.blog;
+
+  return (
+    <div className="blog">
+      <div>
+        <img src={img_url} alt="" />
+      </div>
+      <div className="blog-info">
+        <div className="publish-info">
+          <div>
+            <img src={author_img} alt="" />
+          </div>
+          <div>
+            <div className="author-info">
+              <h5>{author_name}</h5>
+              <p>
+                <small>{publish_date}</small>
+              </p>
             </div>
-            <div className='blog-info'>
-                <div className='publish-info'>
-                    <div>
-                        <img src={author_img} alt="" />
-                    </div>
-                    <div>
-                        <div className='author-info'>
-                            <h5>{author_name}</h5>
-                            <p><small>{publish_date}</small></p>
-                        </div>
-                    </div>
-                </div>
-                <p>{time_to_read} min read <button onClick={() => bookmarked(props.blog)} className='btn-bookmark'><FontAwesomeIcon icon={faBookBookmark} /></button></p>
-            </div>
-            <h2>{title}</h2>
-            <div className='hashtag'>
-                <p>#lovetour</p>
-                <p>#worldtour</p>
-            </div>
-            <button onClick={() => timeCalculation(props.blog)} className='btn-read'>Mark as read</button>
+          </div>
         </div>
-    );
+        <p>
+          {time_to_read} min read{" "}
+          <button
+            onClick={() => handleBookmark(props.blog)}
+            className="btn-bookmark"
+          >
+            <FontAwesomeIcon icon={faBookBookmark} />
+          </button>
+          
+        </p>
+      </div>
+      <h2>{title}</h2>
+      <div className="hashtag">
+        <p>#lovetour</p>
+        <p>#worldtour</p>
+      </div>
+      <button onClick={() => countTime(props.blog.time_to_read)} className="btn-read">
+        Mark as read
+      </button>
+    </div>
+  );
 };
 
 export default Blog;
--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -1,69 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import './Blogs.css'
-import Blog from '../Blog/Blog';
+import React, { useEffect, useState } from "react";
+import "./Blogs.css";
+import Blog from "../Blog/Blog";
 // import Menu from '../Menu/Menu';
-import './Blogs.css'
-import { getStoredCart } from '../../utilities/fakedb';
-
+import "./Blogs.css";
+import { addToDb, getStoredCart } from "../../utilities/fakedb";
+import { Toaster, toast } from "react-hot-toast";
+const notify = () => toast("You Have Already Bookmarked This Blog");
 const Blogs = () => {
-    const [blogs, setBlogs] = useState([]);
-    // const [time, setTime] = useState([]);
-    const [cart, setCart] = useState([]);
-    useEffect( () => {
-        fetch('data.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    }, []);
+  const [blogs, setBlogs] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+  console.log(cart);
 
-    const timeCalculation = (blogs) => {
-        console.log('clicked')
+  const countTime = (time) => {
+    console.log("click", time);
+    setTotalTime(parseFloat(time) + totalTime);
+    localStorage.setItem("spent-time", JSON.stringify(totalTime));
+  };
+
+  const handleBookmark = (selectedBlog) => {
+    let newCart = [];
+    const exists = cart.find((blog) => blog.id === selectedBlog.id);
+    if (!exists) {
+      newCart = [...cart, selectedBlog];
+      setCart(newCart);
+    } else {
+      notify();
+      newCart = [...cart, selectedBlog];
+      setCart(newCart);
     }
+  };
+  console.log(cart);
+  return (
+    <div className="blogs">
+      <div>
+        <Toaster></Toaster>
 
-    const bookmarked = (blog) => {
-        // console.log(blog);
-        const newCart = [...cart, blog]
-        setCart(newCart);
-    }
-
-    useEffect(() => {
-        const storedCart = getStoredCart;
-        const savedCart = [];
-        for (const id in storedCart) {
-          const addedProduct = blogs.find((blog) => blog.id === id);
-          if (addedProduct) {
-            const quantity = storedCart[id];
-            addedProduct.quantity = quantity;
-            savedCart.push(addedProduct);
-          }
-        }
-        setCart(savedCart);
-      }, [blogs]);
-
-    
-    return (
-        <div className='blogs'>
-            <div>
-                {
-                    blogs.map(blog => <Blog
-                    key={blog.id}
-                    blog={blog}
-                    bookmarked={bookmarked}
-                    ></Blog>)
-                }
-            </div>
-            <div className='menu'>
-                <div className='time'>
-                        <p>Spent time on read: <span>0</span> min</p>
-                    </div>
-                    <div className='titles'>
-                        <h5>Bookmarked Blogs: {cart.length}</h5>
-                        <p id='title'>
-
-                        </p>
-                    </div>
-            </div>
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleBookmark={handleBookmark}
+            countTime={countTime}
+          ></Blog>
+        ))}
+      </div>
+      <div className="menu">
+        <div className="time">
+          <p>
+            Spent time on read: <span>{totalTime}</span> min
+          </p>
         </div>
-    );
+        <div className="titles">
+          <h5>Bookmarked Blogs: {cart.length}</h5>
+          {cart.map((item) => (
+            <p id="title">{item.title}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
