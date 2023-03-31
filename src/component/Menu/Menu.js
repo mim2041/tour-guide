0edf0ceefa58c1ebe518dc8072b6b7d42aa1de92
mim2041 <suraiyamim338@gmail.com>
2023-03-31 13:36:02 +0600

    data are fetched and displayed in user interface

--- src/component/Menu/Menu.js
+++ src/component/Menu/Menu.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Menu.css'
 
 const Menu = () => {
     return (
         <div>
-            <div>
+            <div className='time'>
                 <p>Spent time on read: </p>
             </div>
-            <div>
+            <div className='titles'>
                 <h5>Bookmarked Blogs: </h5>
             </div>
         </div>
